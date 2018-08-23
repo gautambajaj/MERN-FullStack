9866b9ba23edaf7cf51114f61a90984226ce5d9c
Gautam Bajaj <gautam.bajaj@uwaterloo.ca>
2018-08-23 17:12:53 +0530

    User registration support added

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
 const app = express();
+
+
+// body-parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+//attach json in post req to res obj
+app.use(bodyParser.json());
 
 // db config
 const db = require('./config/keys').mongoURI;
