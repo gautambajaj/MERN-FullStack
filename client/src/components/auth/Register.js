56083d55539d2b6ad1e3a4718e4dcc3a245d5252
Gautam Bajaj <gautam.bajaj@uwaterloo.ca>
2018-08-28 17:38:05 +0530

    Registration feature added with Redux

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react'
-import axios from 'axios';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 import classnames from 'classnames';
+import { connect } from 'react-redux';
+import { registerUser } from '../../actions/authActions';
 
 class Register extends Component {
 	constructor(){
@@ -14,6 +17,12 @@ class Register extends Component {
 		};
 		this.onChange = this.onChange.bind(this);
 		this.onSubmit = this.onSubmit.bind(this);
+	}
+
+	componentWillReceiveProps(nextProps){
+		if(nextProps.errors){
+			this.setState({errors: nextProps.errors})
+		}
 	}
 
 	onChange(e){
@@ -30,10 +39,7 @@ class Register extends Component {
 			password2: this.state.password2
 		};
 	
-		axios
-			.post('/api/users/register', newUser)
-			.then(res => console.log(res.data))
-			.catch(err => this.setState( {errors: err.response.data} ))
+		this.props.registerUser(newUser, this.props.history);
 	}
 
 	render() {
@@ -116,4 +122,16 @@ class Register extends Component {
 	}	
 }
 
-export default Register;
+// should do to allow access to this.props.registerUser and this.props.auth
+Register.propTypes = {
+	registerUser: PropTypes.func.isRequired,
+	auth: PropTypes.object.isRequired,
+	errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+	auth: state.auth,
+	errors: state.errors
+});
+
+export default connect(mapStateToProps, { registerUser })(withRouter(Register));
