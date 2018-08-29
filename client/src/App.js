4ff9c1c39f539ecb09d69afdde0493163e9d7d51
Gautam Bajaj <gautam.bajaj@uwaterloo.ca>
2018-08-29 16:33:47 +0530

    Create-profile functionality added

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearCurrentProfile } from './actions/profileActions';
 
 import { Provider } from 'react-redux';
 import store from './store';
+
+import PrivateRoute from './components/common/PrivateRoute';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Dashboard from './components/dashboard/Dashboard'
+import CreateProfile from './components/create-profile/CreateProfile'
 
 import './App.css';
 
@@ -34,7 +39,8 @@ if(localStorage.jwtToken){
 		// logout user
 		store.dispatch(logoutUser());
 
-		// TODO: clear current profile
+		// clear current profile
+		store.dispatch(clearCurrentProfile());
 
 		// redirect to login
 		window.location.href = '/login';
@@ -53,6 +59,12 @@ class App extends Component {
 					<div className="container">
 						<Route exact path="/register" component={ Register } />
 						<Route exact path="/login" component={ Login } />
+						<Switch>
+							<PrivateRoute exact path="/dashboard" component={ Dashboard } />
+						</Switch>
+						<Switch>
+							<PrivateRoute exact path="/create-profile" component={ CreateProfile } />
+						</Switch>
 					</div>
 					<Footer/>
 				</div>
