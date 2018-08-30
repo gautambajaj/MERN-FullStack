1e57c3b639bac50e7e407e331076ebd6187bad62
Gautam Bajaj <gautam.bajaj@uwaterloo.ca>
2018-08-30 19:10:30 +0530

    Dashboard component completed

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE } from './types';
+import { GET_PROFILE, GET_ERRORS, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, SET_CURRENT_USER } from './types';
 
 // get current profile
 export const getCurrentProfile = () => dispatch => {
@@ -18,6 +18,106 @@ export const getCurrentProfile = () => dispatch => {
 				payload: {}
 			})		
 		})
+};
+
+// create profile
+export const createProfile = (profileData, history) => dispatch => {
+	axios
+		.post('/api/profile', profileData)
+		.then(res => history.push('/dashboard'))
+		.catch(err => 
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data
+			})
+		);
+};
+
+
+// Add experience 
+export const addExperience = (expData, history) => dispatch => {
+	axios
+		.post('/api/profile/experience', expData)
+		.then(res => history.push('/dashboard'))
+		.catch(err => 
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data
+			})
+		);
+};
+
+
+// Delete experience 
+export const deleteExperience = (id) => dispatch => {
+	axios
+		.delete(`/api/profile/experience/${id}`)
+		.then(res => 
+			dispatch({
+				type: GET_PROFILE,
+				payload: res.data
+			})
+		)
+		.catch(err => 
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data
+			})
+		);
+};
+
+
+// Delete education 
+export const deleteEducation = (id) => dispatch => {
+	axios
+		.delete(`/api/profile/education/${id}`)
+		.then(res => 
+			dispatch({
+				type: GET_PROFILE,
+				payload: res.data
+			})
+		)
+		.catch(err => 
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data
+			})
+		);
+};
+
+
+// Add education 
+export const addEducation = (eduData, history) => dispatch => {
+	axios
+		.post('/api/profile/education', eduData)
+		.then(res => history.push('/dashboard'))
+		.catch(err => 
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data
+			})
+		);
+};
+
+
+// Delete account and profile
+export const deleteAccount = () => dispatch => {
+	if(window.confirm('Are you sure? This action cannot be undone!')){
+		axios
+			.delete('/api/profile')
+			.then(res => 
+				dispatch({
+					type: SET_CURRENT_USER,
+					payload: {}
+				})
+			)
+			.catch(err => 
+				dispatch({
+					type: GET_ERRORS,
+					payload: err.response.data
+				})
+			);
+	}	
 }
 
 // profile loading
@@ -25,11 +125,11 @@ export const setProfileLoading = () => {
 	return {
 		type: PROFILE_LOADING
 	}
-}
+};
 
 // Clear profile
 export const clearCurrentProfile = () => {
 	return {
 		type: CLEAR_CURRENT_PROFILE
 	}
-}
+};
--- client/src/components/common/InputGroup.js
+++ client/src/components/common/InputGroup.js
@@ -15,7 +15,7 @@ const InputGroup = ({
 		<div className="input-group mb-3">
 			<div className="input-group-prepend">
 				<span className="input-group-text">
-					<i classname={icon}></i>
+					<i clasNname={icon}></i>
 				</span>
 			</div>
 	    	<input 
