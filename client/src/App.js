5e9cb1c847c8a2172368aae18d4409b83e3ac5c8
Gautam Bajaj <gautam.bajaj@uwaterloo.ca>
2018-08-31 09:44:15 +0530

    Profile github repos added

--- client/src/App.js
+++ client/src/App.js
@@ -16,11 +16,14 @@ import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
-import Dashboard from './components/dashboard/Dashboard'
-import CreateProfile from './components/create-profile/CreateProfile'
-import EditProfile from './components/edit-profile/EditProfile'
-import AddExperience from './components/add-credentials/AddExperience'
-import AddEducation from './components/add-credentials/AddEducation'
+import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/create-profile/CreateProfile';
+import EditProfile from './components/edit-profile/EditProfile';
+import AddExperience from './components/add-credentials/AddExperience';
+import AddEducation from './components/add-credentials/AddEducation';
+import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
+import NotFound from './components/not-found/NotFound';
 
 import './App.css';
 
@@ -62,6 +65,8 @@ class App extends Component {
 					<div className="container">
 						<Route exact path="/register" component={ Register } />
 						<Route exact path="/login" component={ Login } />
+						<Route exact path="/profiles" component={ Profiles } />
+						<Route exact path="/profile/:handle" component={ Profile } />
 						<Switch>
 							<PrivateRoute exact path="/dashboard" component={ Dashboard } />
 						</Switch>
@@ -77,6 +82,7 @@ class App extends Component {
 						<Switch>
 							<PrivateRoute exact path="/add-education" component={ AddEducation } />
 						</Switch>
+						<Route exact path="/not-found" component={ NotFound } />
 					</div>
 					<Footer/>
 				</div>
